835c12c653ea1234864269af0495dc689aee992a
tohokubussann3700 <tohokubussann@gmail.com>
2026-04-14 00:55:53 +0900

    Add pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import { useState, useEffect, useRef } from 'react'
+import Head from 'next/head'
+
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbxwyfN0X1FW9M_DaFIRWlXxTooOpqJcc1_6O_EB9Nh8kxfRVGolESAF1pvEZuv3wYwnwA/exec'
+
+const DEFAULT_VEHICLES = ['トヨタ ハイエース (品川500あ1234)','横浜300ら5678)' else '全踿】 ]
+export default function Home(){return <div><h1 style={{fontFamily:"noto sans jp,sans-serif",background:"#0f1117",color:"#e8eaf6",minHeight:"100vh"}}>社用車管理ポータル</h1></div>}
